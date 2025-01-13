dcc7e20d071b6bfb600f79aaa4289da36cd5295f
safirasn21 <safirasn021@gmai.com>
2025-01-13 14:50:16 +0700

    Implementasikan InsertMahasiswaView untuk tampilan form dan navigasi

---
+++ app/src/main/java/com/example/firebasepam/ui/view/InsertView.kt
@@ -0,0 +1,103 @@
+package com.example.firebasepam.ui.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebasepam.ui.viewmodel.FormErrorState
+import com.example.firebasepam.ui.viewmodel.FormState
+import com.example.firebasepam.ui.viewmodel.HomeUiState
+import com.example.firebasepam.ui.viewmodel.InsertUiState
+import com.example.firebasepam.ui.viewmodel.InsertViewModel
+import com.example.firebasepam.ui.viewmodel.MahasiswaEvent
+import com.example.firebasepam.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@Composable
+fun InsertMahasiswaView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.uiState
+    val uiEvent = viewModel.uiEvent
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope ()
+
+    LaunchedEffect(uiState) {
+        when (uiState){
+            is FormState.Success -> {
+                println(
+                    "Insert,hsView: uiStet is FormState.Success, navigate to home" + uiState.message
+                )
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+                delay(700)
+                onNavigate()
+                viewModel.resetSnackBarMessage()
+            }
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                title = { Text("Tambah Mahasiswa") },
+                navigationIcon = {
+                    Button(onClick = onBack) {
+                        Text("Back")
+                    }
+                }
+            )
+        }
+    ){ padding ->
+        Column (
+            modifier = modifier.fillMaxSize().padding(padding).padding(16.dp)
+        ){
+            InsertBodyMahasiswa(
+                uiState = uiEvent,
+                homeUiState = uiState,
+                onValueChange = {updatedEvent ->
+            viewModel.updateState(updatedEvent)},
+            onClick = {
+                if (viewModel.validateFields()){
+                    viewModel.insertMahasiswa()
+                }
+            }
+            )
+        }
+    }
+}
+
