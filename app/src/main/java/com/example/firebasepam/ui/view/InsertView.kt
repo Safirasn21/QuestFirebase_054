6c529664f11154172933dedf727b822d69dd12b7
safirasn21 <safirasn021@gmai.com>
2025-01-13 14:51:01 +0700

    Implementasikan InsertBodyMahasiswa untuk form dan tombol submit

--- app/src/main/java/com/example/firebasepam/ui/view/InsertView.kt
+++ app/src/main/java/com/example/firebasepam/ui/view/InsertView.kt
@@ -101,3 +101,40 @@ fun InsertMahasiswaView(
     }
 }
 
+@Composable
+fun InsertBodyMahasiswa (
+    modifier: Modifier = Modifier,
+    onValueChange: (MahasiswaEvent) -> Unit,
+    uiState: InsertUiState,
+    onClick: () -> Unit,
+    homeUiState: FormState
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        FormMahasiswa(
+            mahasiswaEvent = uiState.insertUiEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = modifier.fillMaxWidth(),
+            enabled = homeUiState !is FormState.Loading,
+        ) {
+            if (homeUiState is FormState.Loading){
+                CircularProgressIndicator(
+                    color = Color.White,
+                    modifier = Modifier.size(20.dp).padding(end = 8.dp)
+                )
+                Text("Loading")
+            }else{
+                Text("Add")
+            }
+        }
+    }
+}
+
