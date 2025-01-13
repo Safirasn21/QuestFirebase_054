a41b465a04b7238c9ef046380feda4fd0f1ec23c
safirasn21 <safirasn021@gmai.com>
2025-01-13 14:08:09 +0700

    menambahkan viewmodel untuk insert data mahasiswa

---
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.firebasepam.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebasepam.MahasiswaApplication
+import com.example.firebasepam.model.Mahasiswa
+import com.example.firebasepam.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(
+    private val mhs: MahasiswaRepository
+) : ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+
+
+}
