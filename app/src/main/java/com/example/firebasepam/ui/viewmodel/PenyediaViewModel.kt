7cd40a387806d7b146d6e77adbfea3a8713bb2f9
safirasn21 <safirasn021@gmai.com>
2025-01-13 14:18:46 +0700

    mendaftarkan viewmodel di mahasiswa view model

--- app/src/main/java/com/example/firebasepam/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/PenyediaViewModel.kt
@@ -6,11 +6,14 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.firebasepam.MahasiswaApplication
+import com.example.firebasepam.model.Mahasiswa
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer { HomeViewModel(MahasiswaApplications().container.mahasiswaRepository)}
-
+    initializer { InsertViewModel(
+        MahasiswaApplications().container.mahasiswaRepository
+    ) }
     }
     fun CreationExtras.MahasiswaApplications():MahasiswaApplication =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplication)
