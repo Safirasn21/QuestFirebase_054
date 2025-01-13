60878859d73dca881fc8293bd47c8bd2bb56137b
safirasn21 <safirasn021@gmai.com>
2025-01-13 14:12:09 +0700

    menambahkan sealed class FormState untuk mengelola status form

--- app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
@@ -65,3 +65,10 @@ class InsertViewModel(
     }
 }
 
+sealed class FormState{
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
+
